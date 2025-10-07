3ae1491c1625256a0101748149cc592a864a3935
aymaneiml <aymaneimlihi@gmail.com>
2025-10-07 11:57:39 +0200

    first commit

---
+++ src/test/java/com/springai/prj/chatOllama/ChatOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.prj.chatOllama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
